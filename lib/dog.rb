d57a804aef26c34be60074d0326d94cf8e7acff2
Aaron Feingold <ajfeingold88@gmail.com>
2020-06-08 20:54:17 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,28 +1,28 @@
-# Add your code here
-class Dog
-  attr_accessor :name
+# # Add your code here
+# class Dog
+#   attr_accessor :name
 
-  @@all = []
+#   @@all = []
 
-  def initialize(name)
-    @name = name
-    save
-  end
+#   def initialize(name)
+#     @name = name
+#     save
+#   end
 
-  def self.all
-    @@all
-  end
+#   def self.all
+#     @@all
+#   end
 
-  def self.print_all
-    # puts @@all.map(&:name) - another way to solve, but more complex than needed for this example
-    puts @@all.map{ |dog| dog.name }
-  end
+#   def self.print_all
+#     # puts @@all.map(&:name) - another way to solve, but more complex than needed for this example
+#     puts @@all.map{ |dog| dog.name }
+#   end
 
-  def self.clear_all
-    @@all.clear
-  end
+#   def self.clear_all
+#     @@all.clear
+#   end
 
-  def save
-    @@all << self
-  end
-end
+#   def save
+#     @@all << self
+#   end
+# end
